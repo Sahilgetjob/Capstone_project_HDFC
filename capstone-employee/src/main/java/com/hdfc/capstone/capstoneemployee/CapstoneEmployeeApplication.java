2f59b69117416fc37a05f0a87e43df7541c52319
Sahil_get_job <halosahil@gmail.com>
2023-04-22 00:38:27 +0530

    first commit

---
+++ capstone-employee/src/main/java/com/hdfc/capstone/capstoneemployee/CapstoneEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.hdfc.capstone.capstoneemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneEmployeeApplication.class, args);
+	}
+
+}
