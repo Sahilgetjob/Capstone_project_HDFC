9b08331e76fe114d4dbe06ef280cdb1b7ff30367
Sahil_get_job <halosahil@gmail.com>
2023-04-23 01:01:40 +0530

    sixth commit -> adding client for emp web service

---
+++ client-web-service/src/main/java/com/hdfc/capstone/clientwebservice/ClientWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hdfc.capstone.clientwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientWebServiceApplication.class, args);
+	}
+
+}
---
+++ client-web-service/src/test/java/com/hdfc/capstone/clientwebservice/ClientWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdfc.capstone.clientwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
